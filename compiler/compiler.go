139f6abb4f37bc7596ec60f6129ffd2dc1a2286c
Osezele Orukpe <osezele.o@iflux.app>
2024-02-20 04:57:48 +0100

    Add GlobalScope symbol table and related operations
    Implemented a SymbolTable structure to store and manage global scope symbols, including methods for defining and resolving symbols. Integrated the use of global scope symbols in the compiler code. Accompanied by tests to verify the correct functionality of the SymbolTable structure and operations.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -154,6 +154,11 @@ func (c *Compiler) Compile(node ast.INode) error {
 				return err
 			}
 		}
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
